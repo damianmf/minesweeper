e0a37e17b7c3275815e93fc8e71076bbf0425f01
dmfontenla <damian.fontenla@mercadolibre.com>
2019-10-20 20:55:02 -0300

    Kickoff challenge spring boot minesweeper api

---
+++ src/main/java/com/minesweeper/devigetchallenge/DevigetChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.minesweeper.devigetchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevigetChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevigetChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minesweeper/devigetchallenge/DevigetChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minesweeper.devigetchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevigetChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
